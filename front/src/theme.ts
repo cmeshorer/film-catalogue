3804ee0dbd605b6c7f99e71ca2194f65937aa88f
cmeshorer <cmeshorer@gmail.com>
2024-06-06 14:45:09 +0200

    chore (front): design system

---
+++ front/src/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  blue: "#6573db",
+  green: "#dff7e3",
+  greyscale: {
+    regular: "#c2bebe",
+    white: "#ffff",
+  },
+  status: {
+    error: "#eb4034",
+    warning: "#ffac69",
+  },
+};
